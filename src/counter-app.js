6849907f889e1f09449c0c5f885b1dbe2b19ee0b
djr23523 <141277951+djr23523@users.noreply.github.com>
2024-10-07 03:42:03 -0400

    homework 6

--- src/counter-app.js
+++ src/counter-app.js
@@ -13,6 +13,8 @@ export class counterApp extends DDDSuper(LitElement) {
     this.counter=0;
     this.min=0;
     this.max=0;
+    this.color1=false;
+    this.color2=false;
   }
 
   static get properties() {
@@ -21,6 +23,8 @@ export class counterApp extends DDDSuper(LitElement) {
       counter: { type : Number },
       min: { type : Number },
       max: { type : Number },
+      color1: { type : Boolean, reflect : true},
+      color2: { type : Boolean, reflect : true}
     };
   }
 
@@ -30,9 +34,10 @@ export class counterApp extends DDDSuper(LitElement) {
       :host {
         display: block;
         color: var(--ddd-theme-primary);
-        background-color: var(--ddd-theme-accent);
+        background-color: var(--red-theme-accent);
         font-family: var(--ddd-font-navigation);
         font-size: var(--my-element-font-size, var(--ddd-font-size-s));
+        font-size:2.5rem;
       }
       .wrapper {
         margin: var(--ddd-spacing-2);
@@ -42,25 +47,102 @@ export class counterApp extends DDDSuper(LitElement) {
         padding: 0;
         margin: 0;
       }
+      button:focus{
+        padding:9px;
+        border-radius:10px;
+        background-color:purple;
+      }
+      button:hover{
+        color:pink;
+        background-color:dark green;
+        padding:6px;
+        width:10wh;
+        height:6vh;
+      }
+      .color1{
+        margin: var(--ddd-spacing-2);
+        padding: var(--ddd-spacing-4);
+        color:orange;
+      }
+      .color2{
+        margin: var(--ddd-spacing-2);
+        padding: var(--ddd-spacing-4);
+        color:green;
+      }
+      .color3{
+        margin: var(--ddd-spacing-2);
+        padding: var(--ddd-spacing-4);
+        color:blue;
+      }
+      .color4{
+        margin: var(--ddd-spacing-2);
+        padding: var(--ddd-spacing-4);
+        color:red;
+      }
+      
+      
     `];
   }
   increment(){
     if(this.counter<this.max){
-      this.counter+1;
+      this.counter++;
     }
   }
   decrement(){
     if(this.counter>this.min){
-      this.counter-1;
+      this.counter--;
     }
+  }
+  colorClass(){
+    if(this.counter==18){
+      return "color1"
+    }
+    else if(this.counter==21){
+      return "color2"
+    }
+    else if(this.counter==this.min){
+      return "color3"
+    }
+    else if(this.counter==this.max){
+      return "color4"
+    }
+    else{
+      return "wrapper"
+    }
+  }
+  updated(changedProperties) {
+    if (changedProperties.has('counter')) {
+      this.makeItRain
+    }
+  }
+  
+  makeItRain() {
+    // this is called a dynamic import. It means it won't import the code for confetti until this method is called
+    // the .then() syntax after is because dynamic imports return a Promise object. Meaning the then() code
+    // will only run AFTER the code is imported and available to us
+    import("@haxtheweb/multiple-choice/lib/confetti-container.js").then(
+      (module) => {
+        // This is a minor timing 'hack'. We know the code library above will import prior to this running
+        // The "set timeout 0" means "wait 1 microtask and run it on the next cycle.
+        // this "hack" ensures the element has had time to process in the DOM so that when we set popped
+        // it's listening for changes so it can react
+        setTimeout(() => {
+          // forcibly set the poppped attribute on something with id confetti
+          // while I've said in general NOT to do this, the confetti container element will reset this
+          // after the animation runs so it's a simple way to generate the effect over and over again
+          this.shadowRoot.querySelector("#confetti").setAttribute("popped", "");
+        }, 0);
+      }
+    );
   }
   render() {
     return html`
-<div class="wrapper">
-  <div>${this.title}</div>
-  ${this.counter} <br>
-  <button onclick=increment()>+</button>
-  <button onclick=decrement()>-</button>
+<div class="${this.colorClass()}">
+  <div id="confetti">${this.title}</div>
+  
+    <span>${this.counter} </span><br>
+    <button @click="${this.increment}" ?disabled="${this.max === this.counter}" >+</button>
+    <button @click="${this.decrement}" ?disabled="${this.min === this.counter}">-</button>
 </div>`;
   }
 
